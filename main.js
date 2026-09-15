7cad49f3968513239074a907bfd3ea0f4d0cb08a
Ilhasbi <yaankii03@gmail.com>
2026-09-15 11:06:31 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+// variabel bisa di akses dari variabel lain variable.js file
+console.log(firstName, lastName, country, city, age, isMarried)
+consele.log(gravity, boilingPoint, PI)  // 9.81, 100, 3..14
+console.log(name, job, live)
