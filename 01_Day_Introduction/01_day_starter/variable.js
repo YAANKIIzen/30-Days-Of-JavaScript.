77b88f5685d2bf39ae5ae6175d777de3a7867070
Ilhasbi <yaankii03@gmail.com>
2026-09-15 11:29:31 +0700

    Create variable.js with various variable examples
    Add variable declarations with different data types and values.

---
+++ 01_Day_Introduction/01_day_starter/variable.js
@@ -0,0 +1,17 @@
+// menklarifikasi perbedaan variabel iype data
+let firstname = "Yanki" // nama depan seseorang 
+let lastname = "Hasbi" // nama belakang seseorang 
+let country = "Indonesia" // negara
+let city = "Luwu Timur" // kota/kabupaten
+let age = 44 // umur dalam tahun
+let isMarried = false // pakai boolean
+
+// menklarifikasi variabel dengan angka value
+const gravity = 9.81 // gravitasi bumi dalam m/s2
+const boilingPoint = 100 // titik didih air, temperatur dalm oC
+const PI = 3.14 // nilai phi
+
+//variabel juga bisa menklarifikasi dalam  satu baris terpisah oleh koma
+let name = "Yanki",  // nama seseorang
+  job = "student",
+  live = "Indonesia"
