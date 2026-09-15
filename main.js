7d9bf83d934b6076ea37a50c05a82240379246a0
Ilhasbi <yaankii03@gmail.com>
2026-09-15 19:42:15 +0700

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-// variabel bisa di akses dari variabel lain variable.js file
-console.log(firstName, lastName, country, city, age, isMarried)
-consele.log(gravity, boilingPoint, PI)  // 9.81, 100, 3..14
-console.log(name, job, live)
