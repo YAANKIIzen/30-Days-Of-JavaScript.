1047ad123ca777925eb05d60c898768d8ab0bc93
Ilhasbi <yaankii03@gmail.com>
2026-09-16 12:33:16 +0700

    Create accessing_character.js

---
+++ 02_Day_Data_types/string_methods/accessing_character.js
@@ -0,0 +1,11 @@
+// Mari kita mengakses karakter pertama dalam string "Ilcabbi"
+let string = "Ilcabbi"
+let firstLetter = string[0]
+console.log(firstLetter) // i
+let secondLetter = string[1] // l
+let thirdLetter = string[2] // c
+let lastLetter = string[6] // i
+console.log(lastLetter) // i
+let lastIndex = string.length - 1
+console.log(lastIndex) // 6 
+console.log(string[lastIndex]) // i
