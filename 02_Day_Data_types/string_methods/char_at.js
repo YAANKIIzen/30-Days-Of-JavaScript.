6d16568ecec65fcc1c8dc643d76231bbc18de62f
Ilhasbi <yaankii03@gmail.com>
2026-09-16 18:22:56 +0700

    Create char_at.js

---
+++ 02_Day_Data_types/string_methods/char_at.js
@@ -0,0 +1,6 @@
+// charAt(): Mengambil indeks dan mengembalikan nilai pada indeks tersebut
+let string = "day 2 of javascript"
+let lastIndex = string.length - 1 
+string.charAt(lastIndex) // t
+console.log(string.charAt(0)) // d
+console.log(string.charAt(lastIndex)) // t 
